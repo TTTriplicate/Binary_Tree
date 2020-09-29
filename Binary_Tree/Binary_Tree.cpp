fa634f6b4113df26388d371dc4458d5cd15b4c00
TTTriplicate <tttriplicate@gmail.com>
2020-09-29 07:37:52 -0400

    Add project files.

---
+++ Binary_Tree/Binary_Tree.cpp
@@ -0,0 +1,13 @@
+/*
+Binary_Tree.cpp
+Author: Chris Sousa
+A Binary Tree implementation for 
+algorithmic analysis
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+       
