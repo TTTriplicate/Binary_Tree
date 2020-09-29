a7a7d5b7d5880147afed2eb7288ac9d4aa05c4ef
TTTriplicate <tttriplicate@gmail.com>
2020-09-29 08:21:59 -0400

    Node class complete

--- Binary_Tree/Node.cpp
+++ Binary_Tree/Node.cpp
@@ -1,1 +1,49 @@
 #include "Node.h"
+
+Node::Node() {
+	left = nullptr;
+	right = nullptr;
+}
+
+Node::Node(int data) {
+	data = data;
+	left = nullptr;
+	right = nullptr;
+}
+
+Node::~Node() {
+	delete left;
+	delete right;
+}
+
+int Node::getData() {
+	return data;
+}
+
+Node* Node::getLeft() {
+	return left;
+}
+
+Node* Node::getRight() {
+	return right;
+}
+
+void Node::setLeft(Node* newLeft) {
+	left = newLeft;
+}
+
+void Node::setRight(Node* newRight) {
+	right = newRight;
+}
+
+bool Node::operator<(const Node toCompare) {
+	return data < toCompare.data;
+}
+
+bool Node::operator>(const Node toCompare) {
+	return data > toCompare.data;
+}
+
+bool Node::operator==(const Node toCompare) {
+	return data == toCompare.data;
+}
