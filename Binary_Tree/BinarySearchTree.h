3c9ac6dc39e181c23a87990a0b71578b20b4b445
TTTriplicate <tttriplicate@gmail.com>
2020-09-29 09:06:29 -0400

    BST header filled

--- Binary_Tree/BinarySearchTree.h
+++ Binary_Tree/BinarySearchTree.h
@@ -1,5 +1,26 @@
 #pragma once
+#include "Node.h"
+#include <sstream>
+
 class BinarySearchTree
 {
+private:
+	Node* root;
+	void insert(int data);
+	bool checkBalance();
+	int getHeight();
+	//I chose std::stringstreams to allow main to determine output format
+	//Not a data strucure's job to define how data is output, just stored
+	std::stringstream inOrder();
+	std::stringstream preOrder();
+	std::stringstream postOrder();
+public:
+	BinarySearchTree();
+	~BinarySearchTree();
+	void insert(int data);
+	std::stringstream dataInOrder();
+	std::stringstream dataPreOrder();
+	std::stringstream dataPostOrder();
+	bool isBalanced();
 };
 
