331bfd190a83067c0da79fe299391a19d8c72d5f
TTTriplicate <tttriplicate@gmail.com>
2020-09-29 11:51:34 -0400

    Basics of Tree, working on traversals.

--- Binary_Tree/BinarySearchTree.cpp
+++ Binary_Tree/BinarySearchTree.cpp
@@ -1,1 +1,82 @@
 #include "BinarySearchTree.h"
+
+BinarySearchTree::BinarySearchTree() {
+	root = nullptr;
+}
+
+BinarySearchTree::~BinarySearchTree() {
+	delete root;
+}
+
+void BinarySearchTree::insert(int data) {
+	Node* newNode = new Node(data);
+	if (root == nullptr) {
+		root = newNode;
+	}
+	else {
+		cursor = root;
+		while (true) {
+			if (*(newNode) < *(cursor)) {
+				//insert if less and left is null
+				if (cursor->getLeft() == nullptr) {
+					cursor->setLeft(newNode);
+					break;
+				}
+				else {
+					cursor = cursor->getLeft();
+					continue;
+				}
+			}
+			else {
+				//insert if greater and right is null
+				if (cursor->getRight() == nullptr) {
+					cursor->setRight(newNode);
+					break;
+				}
+				else {
+					cursor = cursor->getRight();
+					continue;
+				}
+			}
+		}
+	}
+}
+
+bool BinarySearchTree::isBalanced() {
+	return checkBalance();
+}
+
+bool BinarySearchTree::checkBalance() {
+	return abs(getHeight(root->getLeft()) - getHeight(root->getRight())) < 2;
+}
+
+int BinarySearchTree::getHeight(Node* node) {
+	if (node == nullptr) {
+		return 0;
+	}
+	else if (node->getLeft() == nullptr && node->getRight() == nullptr) {
+		return 1;
+	}
+	else {
+		int leftHeight = getHeight(node->getLeft());
+		int rightHeight = getHeight(node->getRight());
+		return  1 + max(leftHeight, rightHeight);
+	}
+}
+
+std::stringstream BinarySearchTree::dataPostOrder() {
+	return PostOrder(root);
+}
+
+std::stringstream BinarySearchTree::PostOrder(Node* node) {
+	std::stringstream buffer;
+
+	if (node->getLeft() != nullptr) {
+		buffer << PostOrder(node->getLeft()).rdbuf();
+	}
+	if (node->getRight() != nullptr) {
+		buffer << PostOrder(node->getRight()).rdbuf();
+	}
+	buffer << node->getData();
+	return buffer;
+}
--- Binary_Tree/BinarySearchTree.h
+++ Binary_Tree/BinarySearchTree.h
@@ -6,21 +6,22 @@ class BinarySearchTree
 {
 private:
 	Node* root;
-	void insert(int data);
+	Node* cursor;
 	bool checkBalance();
-	int getHeight();
-	//I chose std::stringstreams to allow main to determine output format
-	//Not a data strucure's job to define how data is output, just stored
-	std::stringstream inOrder();
-	std::stringstream preOrder();
-	std::stringstream postOrder();
+	std::stringstream InOrder(Node* node);
+	std::stringstream PreOrder(Node* node);
+	std::stringstream PostOrder(Node* node);
 public:
 	BinarySearchTree();
 	~BinarySearchTree();
 	void insert(int data);
+	//I chose std::stringstreams to allow main to determine output format
+	//Not a data strucure's job to define how data is output, just stored
 	std::stringstream dataInOrder();
 	std::stringstream dataPreOrder();
 	std::stringstream dataPostOrder();
 	bool isBalanced();
+	int getHeight(Node* node);
+	int max(int a, int b);
 };
 
