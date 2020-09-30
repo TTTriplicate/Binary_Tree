5f4041c5ba97e567b1bad5ce0d4d682069432f60
TTTriplicate <tttriplicate@gmail.com>
2020-09-30 07:51:12 -0400

    Cleaning outputs

--- Binary_Tree/Binary_Tree.cpp
+++ Binary_Tree/Binary_Tree.cpp
@@ -20,24 +20,28 @@ int main()
     plantTree(tree2, "./tree2.txt");
 
 
-    std::cout << tree1.dataPostOrder().str() << std::endl << std::endl;
-    std::cout << tree1.dataPreOrder().str() << std::endl << std::endl;
-    std::cout << tree1.dataInOrder().str() << std::endl << std::endl;
+    std::cout << "Tree 1 in Post Order:\n" << tree1.dataPostOrder().str() << std::endl << std::endl;
+    std::cout << "Tree 1 in Pre Order:\n" << tree1.dataPreOrder().str() << std::endl << std::endl;
+    std::cout << "Tree 1 In Order:\n" << tree1.dataInOrder().str() << std::endl << std::endl;
+
+    std::cout << "Min value in tree 1: " << tree1.getMin()->getData() << std::endl;
+    std::cout << "Max value in tree 1: " << tree1.getMax()->getData() << std::endl;
+
+    std::cout << "Height of tree 1: " << tree1.getHeight() << std::endl;
+    std::string treeBalance = tree1.isBalanced() ? "Tree 1 is balanced" : "Tree 1 is not blanced";
+    std::cout << treeBalance << std::endl << std::endl;
 
 
-    std::cout << tree1.getHeight() << std::endl;
-    std::cout << tree1.isBalanced() << std::endl << std::endl;
+    std::cout << "Tree 2 in Post Order:\n" << tree2.dataPostOrder().str() << std::endl << std::endl;
+    std::cout << "Tree 2 in Pre Order:\n" << tree2.dataPreOrder().str() << std::endl << std::endl;
+    std::cout << "Tree 2 In Order:\n" << tree2.dataInOrder().str() << std::endl << std::endl;
 
+    std::cout << "Min value in tree 2: " << tree2.getMin()->getData() << std::endl;
+    std::cout << "Max value in tree 2: " << tree2.getMax()->getData() << std::endl;
 
-    std::cout << tree2.dataPostOrder().str() << std::endl << std::endl;
-    std::cout << tree2.dataPreOrder().str() << std::endl << std::endl;
-    std::cout << tree2.dataInOrder().str() << std::endl << std::endl;
-
-
-
-    std::cout << tree2.getHeight() << std::endl;
-    std::cout << tree2.isBalanced() << std::endl << std::endl;
-
+    std::cout << "Height of Tree 2: " << tree2.getHeight() << std::endl;
+    treeBalance = tree2.isBalanced() ? "Tree 2 is balanced" : "Tree 2 is not blanced";
+    std::cout << treeBalance << std::endl << std::endl;
 
 }
 
